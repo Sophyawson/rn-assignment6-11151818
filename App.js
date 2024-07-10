b76005d0317a32f028574cc509f98ec1ee0b7426
Sophia <sophieyawson12@gmail.com>
2024-07-10 00:39:13 +0000

    Added the main entry point of the app

--- App.js
+++ App.js
@@ -1,35 +1,63 @@
+import React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
- import { createNativeStackNavigator } from "@react-navigation/native-stack";
- import { SafeAreaProvider } from "react-native-safe-area-context";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeScreen from "./src/components/HomeScreen";
+import CartScreen from "./src/components/CartScreen";
+import ProductDetailScreen from './src/components/ProductDetailScreen';
+import DrawerScreen from './src/components/DrawerScreen';
+import BlogScreen from './src/components/BlogScreen';
+import JewelleryScreen from './src/components/JewelleryScreen';
+import ElectronicScreen from './src/components/ElectronicScreen';
 
- import HomeScreen from "./src/components/HomeScreen";
- import CartScreen from "./src/components/CartScreen";
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 
 const App = () => {
   return (
     <SafeAreaProvider>
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="HomeScreen">
-        <Stack.Screen
-          options={{ headerShown: false }}
-          name="HomeScreen"
-          component={HomeScreen}
-        />
-        <Stack.Screen
-          options={{ headerShown: false }}
-          name="CartScreen"
-          component={CartScreen}
-        />
-        
-      </Stack.Navigator>
-    </NavigationContainer>
-   
+      <NavigationContainer>
+        <Drawer.Navigator 
+          initialRouteName="Store"
+          drawerContent={(props) => <DrawerScreen {...props} />}
+        >
+          <Drawer.Screen
+            options={{ headerShown: false }}
+            name="Store"
+            component={HomeScreen}
+          />
+          <Drawer.Screen
+            options={{ headerShown: false }}
+            name="Location"
+            component={CartScreen}
+          />
+          <Drawer.Screen
+            options={{ headerShown: false }}
+            name="Clothing"
+            component={ProductDetailScreen}
+          />
+            <Drawer.Screen
+            options={{ headerShown: false }}
+            name="BlogScreen"
+            component={BlogScreen}
+          />
+            <Drawer.Screen
+            options={{ headerShown: false }}
+            name="JewelleryScreen"
+            component={JewelleryScreen}
+          />
+            <Drawer.Screen
+            options={{ headerShown: false }}
+            name="ElectronicScreen"
+            component={ElectronicScreen}
+          />
+         
+        </Drawer.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
-    
   );
 };
-export default App;
 
+export default App;
